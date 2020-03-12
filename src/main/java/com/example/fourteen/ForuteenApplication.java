3b082d7608061a11839ae31d298fc2e987ca1f8f
orloveste <orlovevm@yahoo.com>
2020-03-12 11:59:48 +0200

    Dependency Injection - spring - load app when is done

--- src/main/java/com/example/fourteen/ForuteenApplication.java
+++ src/main/java/com/example/fourteen/ForuteenApplication.java
@@ -2,10 +2,20 @@ package com.example.fourteen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ForuteenApplication {
     public static void main(String[] args) {
         SpringApplication.run(ForuteenApplication.class, args);
+    }
+    @Component
+    class Runner{
+        @EventListener(ApplicationReadyEvent.class)
+        public void exercise() throws Exception{
+
+        }
     }
 }
