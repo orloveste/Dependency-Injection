665516e332f76a4380696c2a30ad23221be696f1
orloveste <orlovevm@yahoo.com>
2020-03-12 16:10:48 +0200

    Dependency Injection -spring- Run  MyApplication.java

--- src/main/java/com/legacy/EmailService.java
+++ src/main/java/com/legacy/EmailService.java
@@ -2,6 +2,6 @@ package com.legacy;
 
 public class EmailService {
     public void sendEmail(String message, String reciever){
-        System.out.println("E-mail sent to "+reciever+" with Message="+message);
+        System.out.println("E-mail sent to "+reciever+", with Message= "+message);
     }
 }
---
+++ src/main/java/com/legacy/Runner.java
@@ -0,0 +1,8 @@
+package com.legacy;
+
+public class Runner {
+    public static void main(String[] args) {
+        MyApplication app = new MyApplication();
+        app.processMessages("Mai du-te bre, ca nu te cred", "orlovevm@yahoo.com");
+    }
+}
